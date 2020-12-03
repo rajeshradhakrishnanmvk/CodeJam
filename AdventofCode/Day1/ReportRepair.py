88267f3526711bb66e3682e856ca3f026bff7285
rajeshradhakrishnanmvk <rajeshmvk@gmail.com>
2020-12-03 14:48:21 +0530

    renamed variables

--- AdventofCode/Day1/ReportRepair.py
+++ AdventofCode/Day1/ReportRepair.py
@@ -1,7 +1,7 @@
 with open('input.txt', mode='r') as ip:
-    y = ip.readlines()
+    expense_report = ip.readlines()
 
-thisset = set()
+entries = set()
 i = 0
 
 x = [int(i.strip()) for i in y]
@@ -10,14 +10,14 @@ while i < len(x):
     j = 0
     while j < len(x):
         if (j != i) & (2020 == (x[i] + x[j])):
-            thisset.add(x[i])
-            thisset.add(x[j])
-            print(f"{x[i]},{x[j]}")
+            entries.add(x[i])
+            entries.add(x[j])
+            # print(f"{x[i]},{x[j]}")
         j += 1
     i += 1
 
-result = 1
-for i in thisset:
-    result *= i
+onestar = 1
+for i in entries:
+    onestar *= i
 
-print(result)
+print(onestar)
